ce4c3d739dcb3f614b2583a2d1827b5a33a0bc7d
wyndrick <wyndrick@gmail.com>
2020-03-11 22:02:14 +0200

    - Фикс краша

--- app/src/main/java/com/example/mycameraapp/MainActivity.java
+++ app/src/main/java/com/example/mycameraapp/MainActivity.java
@@ -780,27 +780,49 @@ public class MainActivity extends AppCompatActivity {
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
             mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
             mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            CamcorderProfile cpHigh = CamcorderProfile.get(CamcorderProfile.QUALITY_1080P);
+            Integer[] profiles =  {
+                    CamcorderProfile.QUALITY_1080P,
+                    CamcorderProfile.QUALITY_HIGH,
+                    CamcorderProfile.QUALITY_720P,
+                    CamcorderProfile.QUALITY_480P,
+                    CamcorderProfile.QUALITY_LOW
+            };
+            CamcorderProfile profile = null;
+            for (Integer prof : profiles) {
+                if (CamcorderProfile.hasProfile(prof)) {
+                    profile = CamcorderProfile.get(prof);
+                }
+            }
 
-            mMediaRecorder.setMaxDuration(0);
-            mMediaRecorder.setMaxFileSize(1024 * 1024 * 1024);
-
-
-            mMediaRecorder.setVideoEncodingBitRate(cpHigh.videoBitRate);
-            mMediaRecorder.setVideoFrameRate(cpHigh.videoFrameRate);
-            mMediaRecorder.setVideoSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
-            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-
-            mMediaRecorder.setAudioSamplingRate(cpHigh.audioSampleRate);
-            mMediaRecorder.setAudioChannels(cpHigh.audioChannels);
-            mMediaRecorder.setAudioEncodingBitRate(cpHigh.audioBitRate);
-            mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-            mMediaRecorder.setCaptureRate(30);
             if (mNextVideoAbsolutePath == null || mNextVideoAbsolutePath.isEmpty()) {
                 mNextVideoAbsolutePath = getVideoFilePath(MainActivity.this);
             }
             mMediaRecorder.setOutputFile(mNextVideoAbsolutePath);
+
+
+            mMediaRecorder.setMaxDuration(0);
+            mMediaRecorder.setMaxFileSize(1024 * 1024 * 1024);
+
+            if (null == profile) {
+                mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                mMediaRecorder.setMaxDuration(1000000);
+                mMediaRecorder.setMaxFileSize(500000000);
+                mMediaRecorder.setVideoEncodingBitRate(10000000);
+                mMediaRecorder.setCaptureRate(30);
+                mMediaRecorder.setVideoFrameRate(30);
+            } else {
+                mMediaRecorder.setVideoEncodingBitRate(profile.videoBitRate);
+                mMediaRecorder.setVideoFrameRate(profile.videoFrameRate);
+                mMediaRecorder.setAudioSamplingRate(profile.audioSampleRate);
+                mMediaRecorder.setAudioChannels(profile.audioChannels);
+                mMediaRecorder.setAudioEncodingBitRate(profile.audioBitRate);
+                mMediaRecorder.setCaptureRate(30);
+            }
+
+            mMediaRecorder.setVideoSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            mMediaRecorder.setCaptureRate(30);
 
             int rotation = getWindowManager().getDefaultDisplay().getRotation();
             switch (mSensorOrientation) {
